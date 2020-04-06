9e22370ba2cdeb42ca1f7b211aa2aab4f363abac
Nier <com.owl.minerva@gmail.com>
2020-04-06 21:54:18 +0700

    1

--- app/src/main/java/com/mitratanisejahtera/view/ShowGalleryWebViewActivity.kt
+++ app/src/main/java/com/mitratanisejahtera/view/ShowGalleryWebViewActivity.kt
@@ -1,11 +1,16 @@
 package com.mitratanisejahtera.view
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import android.webkit.*
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.mitratanisejahtera.R
 import com.mitratanisejahtera.model.Url
+import com.mitratanisejahtera.model.Url.webView
 import com.mitratanisejahtera.model.User
 
 class ShowGalleryWebViewActivity : AppCompatActivity() {
@@ -29,6 +34,16 @@ class ShowGalleryWebViewActivity : AppCompatActivity() {
     webContent.settings.javaScriptEnabled = true
     webContent.settings.domStorageEnabled = true
     webContent.settings.javaScriptCanOpenWindowsAutomatically = true
+    webContent.webViewClient = object : WebViewClient() {
+      override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+        val urlString = request.url.toString()
+        if (urlString.contains("/certificate/")) {
+          val intent = Intent(Intent.ACTION_VIEW, request.url)
+          view.context.startActivity(intent)
+        }
+        return true
+      }
+    }
 
     webContent.loadUrl(url, mapOf("Authorization" to "Bearer ${user.token}"))
   }
